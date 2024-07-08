5f3d914f21e7057df56f6f589b94411c3912438a
Keepohkun <Keepohkunnbss2019@gmail.com>
2024-07-08 17:41:31 +0800

    changed things

--- Assets/GameSystem/weapon.cs
+++ Assets/GameSystem/weapon.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+
+    Collider2D weaponCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        weaponCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            //deal dmg
+        }
     }
 }
