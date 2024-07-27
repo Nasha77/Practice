bb0ca06625569cb28833d706c81b0a9b6f61d6c7
Keepohkun <Keepohkunnbss2019@gmail.com>
2024-07-27 19:19:43 +0800

    quest

--- Assets/GameSystem/Data/DataManager.cs
+++ Assets/GameSystem/Data/DataManager.cs
@@ -9,6 +9,7 @@ using static WeaponRef;
 using static EnemyRef;
 using static WaveSpawnerRef;
 using static DialogueRef;
+using static QuestRef;
 
 public class DataManager : MonoBehaviour
 {
@@ -171,6 +172,37 @@ public class DataManager : MonoBehaviour
             dialogueList.Add(thisLine);
         }
         Game.SetDialogueList(dialogueList); // Set dialogue list 
+
+
+
+        // FOR QUEST
+
+        string filePathQuest = Path.Combine(Application.streamingAssetsPath + "/QuestRef.json"); //where to get files from
+
+
+        Debug.Log(filePathQuest);
+
+        string dataStringQuest = File.ReadAllText(filePathQuest); // Read the path and save it in the data string
+        Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" + dataStringQuest);
+        QuestDataList questData = JsonUtility.FromJson<QuestDataList>(dataStringQuest);
+
+        List<Quest> questList = new List<Quest>();
+
+        //process ref data convert data read into classes
+        foreach (QuestRef questref in questData.questRef)
+        {
+            Quest quest = new Quest(
+                questref.questId,
+                questref.questName,
+                questref.questDescription,
+                questref.questReward
+                );
+            questList.Add(quest);
+            Debug.Log("ADD quest " + quest.questName + quest.questReward); //debugger
+
+        }
+        Game.SetQuestList(questList);
+
 
     }
 
