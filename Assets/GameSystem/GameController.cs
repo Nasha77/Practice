a988bbe0f6f86bb6bed3440c7e26c6f2087599eb
Keepohkun <Keepohkunnbss2019@gmail.com>
2024-07-28 13:05:42 +0800

    deathh

--- Assets/GameSystem/GameController.cs
+++ Assets/GameSystem/GameController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public DataManager dataManager; // Call the data
-    public SelectionManager selectionManager; // Reference to SelectionManager script
+    //public SelectionManager selectionManager; // Reference to SelectionManager script
 
     public string initCharacter; // Set in inspector
     public string initWeapon;
@@ -50,5 +50,28 @@ public class GameController : MonoBehaviour
         Debug.Log("NOW " + initWeapon);
         Debug.Log("Character " + Game.GetCharacterList().Count);
         Debug.Log("Weapon " + Game.GetWeaponList().Count);
+    }
+
+
+    // retry button
+    public void RetryButton()
+    {
+        // check if save data exists, if it does then load game scene.
+        // Check if save data exists
+        if (dataManager.LoadPlayerData())
+        {
+            // If save data exists, navigate to the appropriate scene
+            Debug.Log("Save data found, loading gamescene scene.");
+            SceneManager.LoadScene("GameScene");
+        }
+
+        else
+        {
+            // If no save data exists, initialize new player and go to character selection
+            Game.SetPlayer(new Player("1", initCharacter, initWeapon));
+            Game.GetPlayer().GetCurrentCharacter();
+
+
+        }
     }
 }
