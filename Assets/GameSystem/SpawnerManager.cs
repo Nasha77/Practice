2ac5c74f0d877698e2b7d8133f165aee7b8572e1
Keepohkun <Keepohkunnbss2019@gmail.com>
2024-07-28 22:47:30 +0800

    spawner

--- Assets/GameSystem/SpawnerManager.cs
+++ Assets/GameSystem/SpawnerManager.cs
@@ -2,7 +2,6 @@
 
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,43 +10,54 @@ public class SpawnerManager : MonoBehaviour
 
     //set timer to spawn enemies
     public float spawnTimer = 0;
-    // set the interval for enemies to spawn
+    // set the interval between enemies to spawn
     public float spawnInterval = 1;
-    // a way to ref which wave its now
+    // a way to ref current wave
     public int waveIndex = 0;
-    // current wave
+    // keep track of the current wave
     public int currentWaveIndex = 0;
 
-    // by default havent added yet
+    // by default havent added yet, so set to false
     bool enemyAddedToList = false;
 
+    // ref to playermanager
     public PlayerManager playerManager;
 
+    //ref to questmanager
     public QuestManager questManager;
 
+    // instance of questmanager
     public static QuestManager instance;
 
+    // pos of player
     public Transform playerPos;
 
+    // list of the waves
     List<WaveSpawnerRef> waveList = Game.GetWaveList();
-  
 
-    // only declaring the "container", not the "content"
+
+    // this part only declaring the "container", not the "content"
+
+    // Dictionary to pool enemy prefabs
     public Dictionary<string, List<GameObject>> ePrefabPool = new Dictionary<string, List<GameObject>>();
+    // Dictionary to store enemy prefab references
     public Dictionary<string, GameObject> ePrefab = new Dictionary<string, GameObject>();
 
     private void Start()
     {
         // this is causing the extra enemy to be spawned, cuz everytime u start the game, spawning will be true and one will spawn
-       EnemySetUp();
+
+        // Setup enemy prefabs when the game starts
+        EnemySetUp();
     }
 
+    // set up enemy prefabs to be used later
     private void EnemySetUp()
     {
-        //// declaring content for the container
+        // declaring content for the "container"
       
 
-        // adding the prefab into the list you created above
+        // adding the prefab into the dictionary you created above
         ePrefab.Add("e101", Resources.Load<GameObject>("Prefab/FanEnemies/e101"));
         ePrefab.Add("e201", Resources.Load<GameObject>("Prefab/FanEnemies/e201"));
         ePrefab.Add("e301", Resources.Load<GameObject>("Prefab/FanEnemies/e301"));
@@ -62,39 +72,36 @@ public class SpawnerManager : MonoBehaviour
     private void Update()
     {
 
-
+        // exit if no enemies are added
         if (enemyAddedToList == false)
         {
             return;
         }
 
+        // decrement spawn timer
+        // everytime update runs, timer will start decreasing
         spawnTimer -= Time.deltaTime;
 
         // will run code below if enemyaddedtolist is true
-        // code below only runs when its the next wave
+        // code below only runs when its time to spawn the next wave
         // check if its the right time to spawn AND its the same wave. spawn the enemies
         if (spawnTimer < 0 && currentWaveIndex == waveIndex)
         {
-            
 
-            // checking for matching ids
+
+            // Get the enemy to spawn based on the current wave
             Enemy enemyToSpawn = Game.GetEnemyByRefId(waveList[currentWaveIndex].enemyId);
            
-            
-            Debug.Log($"Selected {enemyToSpawn.enemyName}");
-            Debug.Log("NEXT WAVE------------------");
-            Debug.Log(waveList[currentWaveIndex].enemyCount - 1);
 
             // spawn the current wave's enemy and its amount one by one at the right interval
-            // -1 the count cuz at the start it alr spawn one time. so if -1 here, it will spawn the right amount
+            // -1 the enemycount cuz at the start it alr spawn one time. so if -1 here, it will spawn the right amount
             StartCoroutine(SpawnEnemyInterval(enemyToSpawn, waveList[currentWaveIndex].enemyCount-1));
 
             
-            // increase wave idex
+            // after finish spawning all enemies in current wave, increase wave index
             waveIndex++;
-
-            Debug.Log("current wave is at" + currentWaveIndex);
          
+            //reset timer for next wave
             spawnTimer += spawnInterval;
 
             
@@ -103,12 +110,12 @@ public class SpawnerManager : MonoBehaviour
        
     }
 
-    // spawn enemies at a specified interval
+    // coroutine to spawn enemies at a specified interval
     private IEnumerator SpawnEnemyInterval(Enemy enemyToSpawn, int enemiesLeftToSpawn)
     {
        
 
-       // set up the position to be used to spawn enemy
+       // set up the random position to be used to spawn enemy
         Vector2 randomPos = new Vector2(Random.Range(-11.50f, 11.50f), Random.Range(-11.70f, 11.18f));
 
         // spawn the desired enemy at a position 
@@ -118,6 +125,7 @@ public class SpawnerManager : MonoBehaviour
 
 
         // assign health and atk attributes to enemies that will be spawned
+        // basically setting up the enemy's attributes
         enemyObj.GetComponent<EnemyManager>().SetupEnemy(enemyToSpawn, this, FindObjectOfType<PlayerManager>().gameObject);
        
         // wait for awhile before spawning again
@@ -127,20 +135,17 @@ public class SpawnerManager : MonoBehaviour
         // if theres enemies left to spawn, continue spawning
         if (enemiesLeftToSpawn > 0)
         {
-            Debug.Log("enemies left" +enemiesLeftToSpawn);
             StartCoroutine(SpawnEnemyInterval(enemyToSpawn, --enemiesLeftToSpawn));
         }
 
         // if no more left to spawn, go next wave
         else if (enemiesLeftToSpawn == 0)
         {
-            Debug.Log("increased wave index!!!!!!");
             currentWaveIndex++;
 
             // Check if all waves are completed
             if (currentWaveIndex >= waveList.Count)
             {
-                Debug.Log("All waves completed. Checking if all enemies are defeated...");
 
                 // Check if all enemies are defeated
                 StartCoroutine(CheckForWinCondition());
@@ -150,6 +155,7 @@ public class SpawnerManager : MonoBehaviour
 
     }
 
+    // get/ create enemy prefab
     private GameObject GetEnemyPrefab(string enemyId)
     {
         GameObject enemyObj;
@@ -157,7 +163,7 @@ public class SpawnerManager : MonoBehaviour
         // if obj exists in the pool AND if theres enough obj in the pool to be used
         if (ePrefabPool.ContainsKey(enemyId) && ePrefabPool[enemyId].Count > 0)
         {
-            // set obj as the first one
+            // get first available enemy obj
             enemyObj = ePrefabPool[enemyId][0];
 
             //remove it to be used
@@ -173,10 +179,10 @@ public class SpawnerManager : MonoBehaviour
         // if obj doesnt exist in pool and not enough obj
         else
         {
-            // create new list
+            // create new list of enemy obj
             List<GameObject> enemyObjs = new List<GameObject>();
 
-            //use the new list instead
+            //add list to the pool
             ePrefabPool[enemyId] = enemyObjs;
 
             //  instantiate a new enemy object from the prefab
@@ -187,13 +193,17 @@ public class SpawnerManager : MonoBehaviour
         return enemyObj;
     }
 
+    //Return an enemy prefab to the pool
     public void ReturnEnemyPrefab(GameObject enemyObj)
     {
-        // checking if pool exists
+        // basically checking if pool exists for the enemy type 
+        // ePrefabpool dictionary uses enemy object's name as the key
         // check if key doesnt exist, then add pool in
         if (!ePrefabPool.ContainsKey(enemyObj.name))
         {
+            // Create a new pool if it doesn't exist
             // add in the new empty pool
+            
             ePrefabPool.Add(enemyObj.name, new List<GameObject>());
         }
 
@@ -204,6 +214,7 @@ public class SpawnerManager : MonoBehaviour
     }
 
 
+    // Check for win condition after all waves are completed
     private IEnumerator CheckForWinCondition()
     {
         // Wait for a short duration to ensure all enemy deaths are processed
@@ -212,7 +223,6 @@ public class SpawnerManager : MonoBehaviour
         // Check if there are no active enemies left in the scene
         if (FindObjectsOfType<EnemyManager>().Length == 0)
         {
-            Debug.Log("All enemies defeated. You win!");
 
             // Load the WIN scene
             SceneManager.LoadScene("WIN");
