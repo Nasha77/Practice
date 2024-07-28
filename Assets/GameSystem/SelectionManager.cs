e01cb9b79da3f33a2781456c531bd1b17526a44e
Keepohkun <Keepohkunnbss2019@gmail.com>
2024-07-28 22:26:17 +0800

    aaaaaaaaa

--- Assets/GameSystem/SelectionManager.cs
+++ Assets/GameSystem/SelectionManager.cs
@@ -1,105 +1,121 @@
 // KEE POH KUN
 
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor;
 
 public class SelectionManager : MonoBehaviour
 {
-
+    // UI Image component to display character images
     public Image image;
+
+    // store character data
     private List<Character> characterText;
-    private List<Weapon> weapon;
-   
-    public GameObject charaSkin;
+
+  
+    //store sprites
     public List<Sprite> sprite;
+
+    // track current character
     public int characterIndex = 0;
+
+    //UI Text component to display character names
     public TextMeshProUGUI characterName;
 
+    //reference to the SelectionManager instance
     public static SelectionManager instanceRef;
 
+    // SpriteRenderer component to display the player sprite
     public SpriteRenderer playerSprite;
 
 
     void Start()
     {
+        // Singleton pattern to ensure only one instance of SelectionManager exists
         if (instanceRef == null)
         {
             instanceRef = this;
+            // Prevent this object from being destroyed on scene load
             DontDestroyOnLoad(gameObject);
         }
         else if (instanceRef != this)
         {
+            // destroy duplicate instances
             Destroy(gameObject);
         }
+
+        // initialize selection menu
         InitializeMenu();
     }
 
+    // Initialize the character selection menu
     public void InitializeMenu()
     {
+        // Get the list of characters from the game
         characterText = Game.GetCharacterList();
+
+        // Update the UI with the current character information
         UpdateMenuText();
     }
 
+    // Update the UI with the current character information
     private void UpdateMenuText()
     {
-
-        //Debug.Log(characterText[characterIndex].characterName);
         // get data from excel
+        // Set the character name text to the current character's name
         characterName.text = characterText[characterIndex].characterName;
-        
-        
 
+
+        // Set the player sprite to the current character's sprite
         playerSprite.sprite = sprite[characterIndex];
 
     }
 
+    // Select the next character in the list
     public void NextOption()
     {
+        // incrememnt character index
         characterIndex++;
         if(characterIndex == characterText.Count)
         {
+            // Loop back to the first character if the end of the list is reached
             characterIndex = 0;
         }
+        // update UI with new info
         UpdateMenuText();
-
-       // playerSprite.sprite = SetSprite();
 
     }
 
+    // Select the previous character in the list
     public void BackOption()
     {
+        // Decrement the character index
         characterIndex--;
         if(characterIndex < 0)
         {
+            // Loop to the last character if the beginning of the list is reached
             characterIndex = characterText.Count - 1;
         }
+
+        //update with new info
         UpdateMenuText();
 
-        //playerManager.UpdatePlayerStats();
     }
 
+    // go next scene once button plays
     public void PlayOption()
     {
         
-        // Set the current character ID
+        // Set the current character ID for player
         Game.GetPlayer().SetCurrentCharacter(characterText[characterIndex].characterId);
-        Debug.Log("Selected Character ID: " + characterText[characterIndex].characterId);
+        
 
         // Set the current character weapon ID
         Game.GetPlayer().SetCurrentCharacterWeapon(Game.GetWeaponList()[characterIndex].weaponID);
-        Debug.Log("Selected Weapon ID: " + Game.GetWeaponList()[characterIndex].weaponID);
-
-       
-
-        // Save the player data
-        //GameController.instanceRef.dataManager.SavePlayerData();
-
+        
+        // load next scene
         SceneManager.LoadScene("CONVO 2");
     }
 
